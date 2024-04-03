2ae6a896d44cc8d32d664534d18e632feb3155d2
Otavie <otavieokuoyo@gmail.com>
2024-04-03 02:51:36 +0100

    convert buffer to string

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const server = new ws.Server({ port: '3000' })
 
 server.on('connection', socket => {
     socket.on('message', message => {
-        console.log(message)
+        const buffer = Buffer.from(message)
+        console.log(buffer.toString())
         socket.send(`${message}`)
     })
 })
